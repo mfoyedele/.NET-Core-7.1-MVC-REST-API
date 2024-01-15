c7db6391f462f4d9d0328c7823731e6c58f725cf
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-01-15 12:05:17 +0100

    Command model added

---
+++ Commander/Models/Command.cs
@@ -0,0 +1,12 @@
+namespace Commander.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public required string HowTo { get; set; }
+        public required string Line { get; set; }
+
+        public required string Platform { get; set; }
+        
+    }
+}
